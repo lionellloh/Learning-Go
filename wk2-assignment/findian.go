0591cd6077514a5f1b9cd8c728a7de2ec85ed3d1
Lionell Loh Jian An <lionell_loh@mymail.sutd.edu.sg>
2020-02-11 10:58:49 +0800

    findian, string parsing of user input

--- wk2-assignment/findian.go
+++ wk2-assignment/findian.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+func main() {
+	var userString string
+	fmt.Printf("Please enter a string: ")
+	fmt.Scan(&userString)
+	if len(userString) < 3 {
+		fmt.Println("Not Found!")
+		return
+	}
+
+	if ((strings.ToLower(string(userString[0])) == "i") && (strings.ToLower(string(userString[len(userString) -1])) == "n")) {
+		for _, char := range userString {
+			if unicode.ToLower(char) == 'a' {
+				fmt.Println("Found!")
+				return
+			}
+		}
+		fmt.Println("Not Found!")
+		return
+	} else {
+		fmt.Println("Not Found!")
+	}
+}
