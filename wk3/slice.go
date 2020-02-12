87d451f9832202935e88e87b45657609260b2530
Lionell Loh Jian An <lionell_loh@mymail.sutd.edu.sg>
2020-02-12 16:30:41 +0800

    incl some quiz qns

--- wk3/slice.go
+++ wk3/slice.go
@@ -2,32 +2,53 @@ package main
 
 import (
 	"fmt"
-	"sort"
-	"strconv"
 )
 
+//func main() {
+//
+//	arr := make([]int, 3)
+//
+//	for  {
+//		var u string
+//		fmt.Print("Please enter an integer: ")
+//		fmt.Scan(&u)
+//		//fmt.Println(n, err)
+//		if u == "X" {
+//
+//			return
+//		} else{
+//			u, err := strconv.Atoi(u)
+//			if err == nil {
+//				arr = append(arr, u)
+//				sort.Ints(arr)
+//				fmt.Println(arr)
+//			} else {
+//				fmt.Println(err)
+//			}
+//		}
+//	}
+//
+//}
+
+//func main() {
+//	x := [...]int {4, 8, 5}
+//	y := x[0:2]
+//	z := x[1:3]
+//	y[0] = 1
+//	z[1] = 3
+//	fmt.Print(x)
+//}
+
+//func main() {
+//	x := [...]int {1, 2, 3, 4, 5}
+//	y := x[0:2]
+//	z := x[1:4]
+//	fmt.Print(len(y), cap(y), len(z), cap(z))
+//}
+
 func main() {
-
-	arr := make([]int, 3)
-
-	for  {
-		var u string
-		fmt.Print("Please enter an integer: ")
-		fmt.Scan(&u)
-		//fmt.Println(n, err)
-		if u == "X" {
-
-			return
-		} else{
-			u, err := strconv.Atoi(u)
-			if err == nil {
-				arr = append(arr, u)
-				sort.Ints(arr)
-				fmt.Println(arr)
-			} else {
-				fmt.Println(err)
-			}
-		}
-	}
-
+	s := make([]int, 0, 3)
+	s = append(s, 100)
+	fmt.Println(s)
+	fmt.Println(len(s), cap(s))
 }
