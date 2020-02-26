38e351b903ce4edab664b73a8b5887f4f359f637
Lionell Loh Jian An <lionell_loh@mymail.sutd.edu.sg>
2020-02-26 20:32:10 +0800

    dump

--- wk4/makejson.go
+++ wk4/makejson.go
@@ -1,23 +1,23 @@
-//package main
-//
-//import (
-//	"encoding/json"
-//	"fmt"
-//)
-//func main() {
-//	var name string
-//	var address string
-//	person := map[string]string{}
-//	fmt.Printf("Name: ")
-//	fmt.Scan(&name)
-//	fmt.Printf("Address: ")
-//	fmt.Scan(&address)
-//	person["name"] = name
-//	person["address"] = address
-//
-//	data, err := json.Marshal(person)
-//
-//	if err == nil {
-//		fmt.Println(data)
-//	}
-//}
+package wk4
+
+import (
+	"encoding/json"
+	"fmt"
+)
+func main() {
+	var name string
+	var address string
+	person := map[string]string{}
+	fmt.Printf("Name: ")
+	fmt.Scan(&name)
+	fmt.Printf("Address: ")
+	fmt.Scan(&address)
+	person["name"] = name
+	person["address"] = address
+
+	data, err := json.Marshal(person)
+
+	if err == nil {
+		fmt.Println(data)
+	}
+}
--- wk4/read.go
+++ wk4/read.go
@@ -1,4 +1,4 @@
-package main
+package wk4
 
 import (
 	"bufio"
